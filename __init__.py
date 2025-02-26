ddec2f90671418c8e58ac4fc73d70f4371e3b679
gabaaa <gabaaa.dev@gmail.com>
2025-02-26 15:57:30 -0600

    fixed import tests failing

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import os
+direpa_script=os.path.dirname(os.path.realpath(__file__))
+
 from .dev.refine import refine, PathElem as _PathElem
 from .dev.patterns import Pattern as _Pattern, set_pattern as _set_pattern
 from .dev.exceptions import RefineError, RefinePatternError
 
-from .tests.dev.patterns import test_patterns as _test_patterns
-from .tests.dev.path_elems import test_path_elems as _test_path_elems
-from .tests.dev.refine import test_refine as _test_refine
-from .tests.dev.sources import test_sources as _test_sources
+if os.path.exists(os.path.join(direpa_script, "tests")):
+    from .tests.dev.patterns import test_patterns as _test_patterns
+    from .tests.dev.path_elems import test_path_elems as _test_path_elems
+    from .tests.dev.refine import test_refine as _test_refine
+    from .tests.dev.sources import test_sources as _test_sources
